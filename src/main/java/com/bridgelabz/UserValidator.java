38f25f03111bfe51526c6c004e214a9edc123900
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-15 20:22:20 +0530

    Refactor: comments added

--- src/main/java/com/bridgelabz/UserValidator.java
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -4,9 +4,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidator {
+
+    //*Regex Pattern to Check First Name
+    // 1st Later caps and minimum 3 chars*//
     private final String FIRST_LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,10}";
     Pattern pattern;
 
+    //*Method to validate first name
+    // taking pattern as regex string and matcher as first name parameter to check
+    // result in boolean format if match true else false
+    // return this result back to FirstNameTest.java*//
     public boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(FIRST_LAST_NAME_PATTERN);
         Matcher matcher = pattern.matcher(firstName);
--- src/test/java/com/bridgelabz/FirstNameTest.java
+++ src/test/java/com/bridgelabz/FirstNameTest.java
@@ -8,16 +8,21 @@ import org.junit.runners.Parameterized;
 import java.util.ArrayList;
 import java.util.List;
 
+/*To Declare the class will be Parameterized class*/
 @RunWith(Parameterized.class)
 public class FirstNameTest {
+
+    //*initializing fields firstname and expected(result boolean)*//
     String firstName;
     boolean expected;
 
+    //*Constructor created to provide current object value*//
     public FirstNameTest(String firstName, boolean result) {
         this.firstName = firstName;
         this.expected = result;
     }
 
+    /*Parameters for class in list format is provided in the inputFirst method*/
     @Parameterized.Parameters
     public static List inputFirst(){
         List list = new ArrayList();
@@ -27,6 +32,8 @@ public class FirstNameTest {
         return list;
     }
 
+    //*Object of validatorFirstName class created
+    // checking validateFirstName methods result with expected result using equals method*//
     @Test
     public void validateFirstName() {
         UserValidator userValidator = new UserValidator();
